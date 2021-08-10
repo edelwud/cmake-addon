eba3b6305c73c4b69c674d695fe096b20d0503d8
edelwud <edelwud@icloud.com>
2021-08-10 19:25:17 +0300

    remove: module installer to project init

--- module/install.cmake
+++ module/install.cmake
@@ -5,43 +5,43 @@ macro(module_install_headers _include_dir)
     ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-    INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-    PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+    INCLUDES
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${ADDON_APP}
+    PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${ADDON_APP}
   )
-  install(DIRECTORY ${_include_dir} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+  install(DIRECTORY ${_include_dir} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${ADDON_APP})
 endmacro()
 
 macro(module_project_properties)
-  set_target_properties(${PROJECT_NAME} PROPERTIES
-    VERSION ${PROJECT_VERSION}
-    SOVERSION ${PROJECT_VERSION_MAJOR}
-    INTERFACE_${PROJECT_NAME}_MAJOR_VERSION ${PROJECT_VERSION_MAJOR}
-    COMPATIBLE_INTERFACE_STRING ${PROJECT_VERSION_MAJOR}
+  set_target_properties(
+    ${PROJECT_NAME}
+    PROPERTIES VERSION ${PROJECT_VERSION}
+               SOVERSION ${PROJECT_VERSION_MAJOR}
+               INTERFACE_${PROJECT_NAME}_MAJOR_VERSION ${PROJECT_VERSION_MAJOR}
+               COMPATIBLE_INTERFACE_STRING ${PROJECT_VERSION_MAJOR}
   )
 endmacro()
 
-macro(module_install_cmake_config _include_dir)
+macro(modules_install_cmake_config)
+  module_project_properties()
+
+  export(EXPORT ${ADDON_APP}Targets FILE "${ADDON_MODULE_PACKAGES_DIR}/${PROJECT_NAME}.cmake")
+
   include(CMakePackageConfigHelpers)
 
-  write_basic_package_version_file(
-    "${ADDON_MODULE_PACKAGES_DIR}/${ADDON_APP}ConfigVersion.cmake"
-    VERSION ${PROJECT_VERSION}
-    COMPATIBILITY SameMajorVersion)
+  get_all_targets(all_targets)
 
-  export(EXPORT ${ADDON_APP}Targets
-         FILE "${ADDON_MODULE_PACKAGES_DIR}/${PROJECT_NAME}.cmake")
-
-  set(CONFIG_SOURCE_DIR ${CMAKE_SOURCE_DIR})
-  set(CONFIG_DIR ${CMAKE_BINARY_DIR})
-  set(${PROJECT_NAME}_INCLUDE_DIR "${_include_dir}")
   configure_package_config_file(
     ${CMAKE_SOURCE_DIR}/${ADDON_DIRNAME}/templates/module.cmake.in
     "${ADDON_MODULE_PACKAGES_DIR}/${ADDON_APP}Config.cmake"
     INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${ADDON_APP}
-    PATH_VARS CMAKE_INSTALL_LIBDIR
   )
 
-  module_project_properties()
+  write_basic_package_version_file(
+    "${ADDON_MODULE_PACKAGES_DIR}/${ADDON_APP}ConfigVersion.cmake"
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY SameMajorVersion
+  )
 
   install(
     EXPORT ${ADDON_APP}Targets
@@ -53,10 +53,7 @@ macro(module_install_cmake_config _include_dir)
     FILES "${ADDON_MODULE_PACKAGES_DIR}/${ADDON_APP}Config.cmake"
           "${ADDON_MODULE_PACKAGES_DIR}/${ADDON_APP}ConfigVersion.cmake"
     DESTINATION lib/cmake/${ADDON_APP}
-    COMPONENT ${PROJECT_NAME}
   )
-
-  export(PACKAGE ${PROJECT_NAME})
 endmacro()
 
 macro(module_install_pkg_config)
@@ -64,18 +61,19 @@ macro(module_install_pkg_config)
   set(libdir ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR})
   set(prefix ${CMAKE_INSTALL_PREFIX})
 
-  configure_file(${ADDON_PATH}/templates/module.pc.in
-                 ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_NAME}.pc @ONLY)
+  configure_file(
+    ${ADDON_PATH}/templates/module.pc.in ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_NAME}.pc @ONLY
+  )
 
   install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_NAME}.pc
-          DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig)
+          DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig
+  )
 endmacro()
 
 macro(module_install _include_dir _pkgconf _cmake)
-  module_install_headers(${_include_dir})
   if(${MODULE_TYPE} MATCHES library)
     if(${_cmake})
-      module_install_cmake_config(${_include_dir})
+      module_install_headers(${_include_dir})
     endif()
     if(${_pkgconf})
       module_install_pkg_config(${ARGN})
